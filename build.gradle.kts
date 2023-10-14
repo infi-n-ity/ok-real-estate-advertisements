288b94d33fcfedf1e11b190049a3c8f61b07954b
Andrey <andrey.lukyanchenko01@gmail.com>
2023-10-14 21:43:36 +0600

    ДЗ1 Структура проекта

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm")
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+subprojects {
+    group = "ok-real-estate-advertisements.infi-n-ity.github.com"
+    version = "1.0-SNAPSHOT"
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
